0a9ffaddbb85b8fa86ed9dcaaf40e9aa90b2c7e0
Gangadhar-05 <gangadhar.2k24@gmail.com>
2026-06-17 22:44:38 +0530

    Longest_subarray_with_sum_K

---
+++ Arrays/Longest_subarray_with_sum_K.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestSubarray(int[] nums, int k) {
+        int n=nums.length;
+        int left=0;
+        int right=0;
+        int sum = nums[0];
+        int max_length=0;
+        while(right<n){
+
+            while(left<=right && sum>k){
+                sum-=nums[left];
+                left++;
+            }
+
+            if(sum==k){
+                max_length=Math.max(max_length,right-left+1);
+            }
+
+            right++;
+            if(right<n){
+                sum+=nums[right];
+            }
+        }
+       return max_length;
+    }
+}
